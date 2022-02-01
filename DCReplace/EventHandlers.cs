d834c9e2b70321ecfe88e60b7a6eb3966b896740
Cyanox62 <cyanox62@gmail.com>
2022-01-31 21:54:26 -0500

    added support for 966 and fixed the mess of copy paste code

--- DCReplace/EventHandlers.cs
+++ DCReplace/EventHandlers.cs
@@ -94,6 +94,117 @@ namespace DCReplace
 			}
 		}
 
+		private void ReplacePlayer(Player player)
+		{
+			bool is035 = false;
+			bool isSH = false;
+			if (isContain106 && player.Role == RoleType.Scp106) return;
+			Dictionary<Player, bool> spies = null;
+
+			try
+			{
+				is035 = TryGet035().Contains(player);
+			}
+			catch (Exception x)
+			{
+				Log.Error(x);
+				Log.Debug("SCP-035 is not installed, skipping method call...");
+			}
+
+			try
+			{
+				isSH = TryGetSH().Contains(player);
+			}
+			catch (Exception x)
+			{
+				Log.Debug("Serpents Hand is not installed, skipping method call...");
+			}
+
+			try
+			{
+				spies = TryGetSpies();
+			}
+			catch (Exception x)
+			{
+				Log.Debug("CISpy is not installed, skipping method call...");
+			}
+
+			Player replacement = Player.List.FirstOrDefault(x => x.Role == RoleType.Spectator && x.Id != player.Id && !x.IsOverwatchEnabled);
+			if (replacement != null)
+			{
+				PositionsToSpawn.Add(replacement, player.Position);
+				if (isSH)
+				{
+					try
+					{
+						TrySpawnSH(replacement);
+					}
+					catch (Exception x)
+					{
+						Log.Debug("Serpents Hand is not installed, skipping method call...");
+					}
+				}
+				else replacement.SetRole(player.Role);
+				if (spies != null && spies.ContainsKey(player))
+				{
+					try
+					{
+						TrySpawnSpy(replacement, player, spies);
+					}
+					catch (Exception x)
+					{
+						Log.Debug("CISpy is not installed, skipping method call...");
+					}
+				}
+				if (is035)
+				{
+					try
+					{
+						TrySpawn035(replacement);
+					}
+					catch (Exception x)
+					{
+						Log.Debug("SCP-035 is not installed, skipping method call...");
+					}
+				}
+
+				if ((string)Loader.Plugins.First(pl => pl.Name == "scp966")?.Assembly?.GetType("scp966.API.Scp966API")?.GetMethod("GetLastScp966", BindingFlags.Public | BindingFlags.Static)?.Invoke(null, null) == player.UserId)
+				{
+					Loader.Plugins.First(pl => pl.Name == "scp966").Assembly.GetType("scp966.API.Scp966API").GetMethod("Spawn966", BindingFlags.Public | BindingFlags.Static).Invoke(null, new object[] { replacement });
+				}
+
+				// save info
+				var inventory = player.Items.Select(x => x.Type).ToList();
+				float health = player.Health;
+				byte scp079lvl = 1;
+				float scp079exp = 0f;
+				if (player.Role == RoleType.Scp079)
+				{
+					scp079lvl = player.ReferenceHub.scp079PlayerScript.Lvl;
+					scp079exp = player.ReferenceHub.scp079PlayerScript.Exp;
+				}
+				Dictionary<ItemType, ushort> ammo = new Dictionary<ItemType, ushort>();
+				foreach (ItemType ammoType in player.Ammo.Keys) ammo.Add(ammoType, player.Ammo[ammoType]);
+
+				Timing.CallDelayed(0.5f, () =>
+				{
+					replacement.ResetInventory(inventory);
+					replacement.Health = health;
+					foreach (ItemType ammoType in ammo.Keys)
+					{
+						replacement.Inventory.UserInventory.ReserveAmmo[ammoType] = ammo[ammoType];
+						replacement.Inventory.SendAmmoNextFrame = true;
+					}
+					replacement.Broadcast(5, "<i>You have replaced a player who has disconnected.</i>");
+					if (replacement.Role == RoleType.Scp079)
+					{
+						replacement.ReferenceHub.scp079PlayerScript.Lvl = scp079lvl;
+						replacement.ReferenceHub.scp079PlayerScript.Exp = scp079exp;
+					}
+				});
+			}
+		}
+
 		public void OnRoundStart()
 		{
 			isContain106 = false;
@@ -119,218 +230,14 @@ namespace DCReplace
 			if (ev.Reason != (SpawnReason)120) return;
 			if (!isRoundStarted || ev.Player.Role == RoleType.Spectator || ev.Player.Role == RoleType.None || ev.Player.Position.y < -1997 || (ev.Player.CurrentRoom.Zone == ZoneType.LightContainment && Map.IsLczDecontaminated)) return;
 
-			bool is035 = false;
-			bool isSH = false;
-			if (isContain106 && ev.Player.Role == RoleType.Scp106) return;
-			Dictionary<Player, bool> spies = null;
-			var role = Loader.Plugins.FirstOrDefault(pl => pl.Name == "EasyEvents")?.Assembly.GetType("EasyEvents.Util")?.GetMethod("GetRole")?.Invoke(null, new object[] { ev.Player });
-			try
-			{
-				is035 = TryGet035().Contains(ev.Player);
-			}
-			catch (Exception x)
-			{
-				Log.Error(x);
-				Log.Debug("SCP-035 is not installed, skipping method call...");
-			}
-
-			try
-			{
-				isSH = TryGetSH().Contains(ev.Player);
-			}
-			catch (Exception x)
-			{
-				Log.Debug("Serpents Hand is not installed, skipping method call...");
-			}
-
-			try
-			{
-				spies = TryGetSpies();
-			}
-			catch (Exception x)
-			{
-				Log.Debug("CISpy is not installed, skipping method call...");
-			}
-
-			Player player = Player.List.FirstOrDefault(x => x.Role == RoleType.Spectator && x.Id != ev.Player.Id && !x.IsOverwatchEnabled);
-			if (player != null)
-			{
-				PositionsToSpawn.Add(player, ev.Player.Position);
-				if (isSH)
-				{
-					try
-					{
-						TrySpawnSH(player);
-					}
-					catch (Exception x)
-					{
-						Log.Debug("Serpents Hand is not installed, skipping method call...");
-					}
-				}
-				else player.SetRole(ev.Player.Role);
-				if (spies != null && spies.ContainsKey(ev.Player))
-				{
-					try
-					{
-						TrySpawnSpy(player, ev.Player, spies);
-					}
-					catch (Exception x)
-					{
-						Log.Debug("CISpy is not installed, skipping method call...");
-					}
-				}
-				if (is035)
-				{
-					try
-					{
-						TrySpawn035(player);
-					}
-					catch (Exception x)
-					{
-						Log.Debug("SCP-035 is not installed, skipping method call...");
-					}
-				}
-
-				// save info
-				var inventory = ev.Player.Items.Select(x => x.Type).ToList();
-				float health = ev.Player.Health;
-				byte scp079lvl = 1;
-				float scp079exp = 0f;
-				if (ev.Player.Role == RoleType.Scp079)
-				{
-					scp079lvl = ev.Player.ReferenceHub.scp079PlayerScript.Lvl;
-					scp079exp = ev.Player.ReferenceHub.scp079PlayerScript.Exp;
-				}
-				Dictionary<ItemType, ushort> ammo = new Dictionary<ItemType, ushort>();
-				foreach (ItemType ammoType in ev.Player.Ammo.Keys) ammo.Add(ammoType, ev.Player.Ammo[ammoType]);
-
-				Timing.CallDelayed(0.5f, () =>
-				{
-					player.ResetInventory(inventory);
-					player.Health = health;
-					foreach (ItemType ammoType in ammo.Keys)
-					{
-						player.Inventory.UserInventory.ReserveAmmo[ammoType] = ammo[ammoType];
-						player.Inventory.SendAmmoNextFrame = true;
-					}
-					player.Broadcast(5, "<i>You have replaced a player who has disconnected.</i>");
-					if (player.Role == RoleType.Scp079)
-					{
-						player.ReferenceHub.scp079PlayerScript.Lvl = scp079lvl;
-						player.ReferenceHub.scp079PlayerScript.Exp = scp079exp;
-					}
-					if (role != null) Loader.Plugins.FirstOrDefault(pl => pl.Name == "EasyEvents")?.Assembly.GetType("EasyEvents.CustomRoles")?.GetMethod("ChangeRole")?.Invoke(null, new object[] { player, role });
-				});
-			}
+			ReplacePlayer(ev.Player);
 		}
 
 		public void OnPlayerLeave(LeftEventArgs ev)
 		{
 			if (!isRoundStarted || ev.Player.Role == RoleType.Spectator || ev.Player.Role == RoleType.None || ev.Player.Position.y < -1997 || (ev.Player.CurrentRoom.Zone == ZoneType.LightContainment && Map.IsLczDecontaminated)) return;
 
-			bool is035 = false;
-			bool isSH = false;
-			if (isContain106 && ev.Player.Role == RoleType.Scp106) return;
-			Dictionary<Player, bool> spies = null;
-			var role = Loader.Plugins.FirstOrDefault(pl => pl.Name == "EasyEvents")?.Assembly.GetType("EasyEvents.Util")?.GetMethod("GetRole")?.Invoke(null, new object[] {ev.Player});
-			try
-			{
-				is035 = TryGet035().Contains(ev.Player);
-			}
-			catch (Exception x)
-			{
-				Log.Error(x);
-				Log.Debug("SCP-035 is not installed, skipping method call...");
-			}
-
-			try
-			{
-				isSH = TryGetSH().Contains(ev.Player);
-			}
-			catch (Exception x)
-			{
-				Log.Debug("Serpents Hand is not installed, skipping method call...");
-			}
-
-			try
-			{
-				spies = TryGetSpies();
-			}
-			catch (Exception x)
-			{
-				Log.Debug("CISpy is not installed, skipping method call...");
-			}
-
-			Player player = Player.List.FirstOrDefault(x => x.Role == RoleType.Spectator && x.Id != ev.Player.Id && !x.IsOverwatchEnabled);
-			if (player != null)
-			{
-				PositionsToSpawn.Add(player, ev.Player.Position);
-				if (isSH)
-				{
-					try
-					{
-						TrySpawnSH(player);
-					}
-					catch (Exception x)
-					{
-						Log.Debug("Serpents Hand is not installed, skipping method call...");
-					}
-				}
-				else player.SetRole(ev.Player.Role);
-				if (spies != null && spies.ContainsKey(ev.Player))
-				{
-					try
-					{
-						TrySpawnSpy(player, ev.Player, spies);
-					}
-					catch (Exception x)
-					{
-						Log.Debug("CISpy is not installed, skipping method call...");
-					}
-				}
-				if (is035)
-				{
-					try
-					{
-						TrySpawn035(player);
-					}
-					catch (Exception x)
-					{
-						Log.Debug("SCP-035 is not installed, skipping method call...");
-					}
-				}
-
-				// save info
-				var inventory = ev.Player.Items.Select(x => x.Type).ToList();
-				float health = ev.Player.Health;
-				byte scp079lvl = 1;
-				float scp079exp = 0f;
-				if (ev.Player.Role == RoleType.Scp079)
-				{
-					scp079lvl = ev.Player.ReferenceHub.scp079PlayerScript.Lvl;
-					scp079exp = ev.Player.ReferenceHub.scp079PlayerScript.Exp;
-				}
-				Dictionary<ItemType, ushort> ammo = new Dictionary<ItemType, ushort>();
-				foreach (ItemType ammoType in ev.Player.Ammo.Keys) ammo.Add(ammoType, ev.Player.Ammo[ammoType]);
-
-				Timing.CallDelayed(0.5f, () =>
-				{
-					player.ResetInventory(inventory);
-					player.Health = health;
-					foreach (ItemType ammoType in ammo.Keys)
-					{
-						player.Inventory.UserInventory.ReserveAmmo[ammoType] = ammo[ammoType];
-						player.Inventory.SendAmmoNextFrame = true;
-					}
-					player.Broadcast(5, "<i>You have replaced a player who has disconnected.</i>");
-					if (player.Role == RoleType.Scp079)
-                    {
-						player.ReferenceHub.scp079PlayerScript.Lvl = scp079lvl;
-						player.ReferenceHub.scp079PlayerScript.Exp = scp079exp;
-					}
-					if(role != null) Loader.Plugins.FirstOrDefault(pl => pl.Name == "EasyEvents")?.Assembly.GetType("EasyEvents.CustomRoles")?.GetMethod("ChangeRole")?.Invoke(null, new object[] {player, role});
-				});
-			}
+			ReplacePlayer(ev.Player);
 		}
 	}
 }
